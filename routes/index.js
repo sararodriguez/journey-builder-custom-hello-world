b11ab5980c1dd4a17574860f12669b1bbccb463f
Sara Rodriguez <sarah0neko@gmail.com>
2017-10-05 11:31:36 -0500

    Mensaje de error

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ exports.index = function(req, res){
     if( !req.session.token ) {
         res.render( 'index', {
             title: 'Unauthenticated',
-            errorMessage: 'This app may only be loaded via the ExactTarget Marketing Cloud',
+            errorMessage: 'This app may only be loaded via the ExactTarget Marketing Cloud - Sara',
         });
     } else {
         res.render( 'index', {
